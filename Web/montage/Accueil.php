ba83b8ac5e7ffada04dacac50d058b84681d25cb
MaximeSaliou <maxime.saliou@bts-malraux.net>
2016-04-27 14:37:25 +0200

    mise a jour

--- Web/montage/Accueil.php
+++ Web/montage/Accueil.php
@@ -20,11 +20,12 @@ and open the template in the editor.
                     ?>
 
            <div id="cadre">
+             <!-- Permet de sélectionner la vidéo auquel on veut afficher les détail des rushes -->
              <h3> Détail Vidéo </h3>
                <select name="nomvideo" required="">
                  <option value="">Sélectionnez une vidéo</option>
                  <?php
-                 $req = "SELECT * FROM video;";
+                 $req = "SELECT * FROM Video;";
                  $resultat =$connexion->query($req);
                  while ($ligne= $resultat->fetch()) {
                    echo "<option value=''>".$ligne[nom]."</option>";
@@ -34,6 +35,21 @@ and open the template in the editor.
                  ?>
                </select>
                  <input type="submit" value="Valider"/>
+
+                 <?php
+
+                 if (isset($_POST['nom'])) {
+
+
+                    $res = mysql_query("SELECT * FROM Rushes WHERE nom='".mysql_real_escape_string($_POST['nom'])."'");
+                    echo '<table><tr><th>nom</th><th>timIn</th><th>timOut</th></tr>';
+                    while ($ligne = mysql_fetch_assoc($resultat->fetch())) {
+                       echo '<tr><td>',$row['nom'],'</td><td>', $row['timeIn'], ', ', $row['timeOut'], '</td></tr>';
+                    }
+                    echo '</table>';
+                 }
+                 ?>
+
            </div>
 
 
--- Web/montage/Include/ConnexionmBDD.php
+++ Web/montage/Include/ConnexionmBDD.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$PARAM_hote = 'localhost';
-$PARAM_port = '';
-$PARAM_nom_bd = 'test';
-$PARAM_utilisateur = 'root';
-$PARAM_mot_passe = '';
+$PARAM_hote = '172.16.99.3';
+$PARAM_port = '3306';
+$PARAM_nom_bd = 'm.saliou';
+$PARAM_utilisateur = 'm.saliou';
+$PARAM_mot_passe = 'P@ssword';
 $connexion = new PDO('mysql:host='.$PARAM_hote.';port='.$PARAM_port.';dbname='.$PARAM_nom_bd, $PARAM_utilisateur, $PARAM_mot_passe);
 ?>
